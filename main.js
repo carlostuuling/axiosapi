3dda8fb9db189298a11d8243fa8a1181ea04ed0f
Carlos <Carlos.tuuling@ametikool.ee>
2022-10-06 13:02:39 +0300

    make ready stuff yes happy yes

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ let dropdown = document.getElementById("countries");
 dropdown.addEventListener('input', async (e) => {
     let country = e.target.value;
     console.log(country)
+
     try {
         await API.get('statistics', {params: {country:country}})
         .then(response => {
             console.log(response.data.response[0]);
+            let infoDiv = document.getElementById('infoDiv')
+            infoDiv.innerHTML = '<p> Population: ' + response.data.response[0].population + '</p>' + '<p> Cases: ' +
+            response.data.response[0].cases.active + '</p>' + '<p> continent: ' + response.data.response[0].continent + '</p>' +
+            '<p> Deaths: ' + response.data.response[0].deaths.total + '</p>'
+
+
         })
     } catch (error) {
         console.error(error);
@@ -22,7 +29,9 @@ try {
             let option = document.createElement("option");
             option.innerHTML = country;
             dropdown.appendChild(option);
+
         });
+
     })
 } catch (error) {
     console.error(error);
